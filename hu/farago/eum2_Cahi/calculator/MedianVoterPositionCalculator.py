c1649bda457c6fb44ed1897684e0fb245c93e950
octogon21 <czachesz@otpalap.hu>
2016-06-13 12:13:54 +0200

    Cahi implementation

---
+++ hu/farago/eum2_Cahi/calculator/MedianVoterPositionCalculator.py
@@ -0,0 +1,65 @@
+'''
+Created on 2016 máj. 19
+
+@author: Balázs
+'''
+
+from typing import Iterable
+from hu.farago.eum2_Cahi.dto.Player import Player
+from hu.farago.eum2_Cahi.calculator.Helper import tablePrint
+
+class MedianVoterPositionCalculator():
+
+    __players = []
+    __positionMin = 0
+    __positionMax = 0
+    
+    __positionMaxDifference = 0
+    __medianVoterPosition = None
+
+    def __init__(self, players:Iterable[Player]):
+        self.__players = players
+
+    def getPositionMaxDifference(self):
+        return self.__positionMaxDifference
+    
+    def getMedianVoterPosition(self):
+        return self.__medianVoterPosition
+
+    def calculateMedianVoterPosition(self):
+        def playerPos(p):
+            return p.position
+        
+        # self.__players.sort(key=playerPos)
+        self.__positionMin = min(self.__players, key=playerPos).position
+        self.__positionMax = max(self.__players, key=playerPos).position
+        self.__positionMaxDifference = abs(self.__positionMax - self.__positionMin)
+        
+        if self.__positionMaxDifference == 0:
+            return
+        
+        length = len(self.__players)
+        votesForJVersusK = [[0 for x in range(length)] for y in range(length)]
+        
+        for j, playerJ in enumerate(self.__players):
+            for k, playerK in enumerate(self.__players):
+                sumOfVotes = 0
+                for playerI in self.__players:
+                    if playerI != playerJ and playerI != playerK:
+                        sumOfVotes += self.voteBetweenPlayers(playerI, playerJ, playerK)
+                
+                votesForJVersusK[j][k] = sumOfVotes
+                playerJ.addToSum(sumOfVotes)
+                
+        #print("=========== votes for J vs K =============")        
+        #tablePrint(votesForJVersusK)
+        
+        self.__medianVoterPosition = max(self.__players, key=lambda x: x.sumOfVotes)
+        
+    def voteBetweenPlayers(self, voteI: Player, voteJ: Player, voteK: Player):
+        diffIK = abs(voteI.position - voteK.position)
+        diffIJ = abs(voteI.position - voteJ.position)
+        
+        #return voteI.power()*((diffIK - diffIJ)/self.__positionMaxDifference)        
+        return 2*voteI.power()*((diffIK - diffIJ)/self.__positionMaxDifference)
+        
---
+++ hu/farago/eum2_Cahi/dto/Player.py
@@ -0,0 +1,45 @@
+'''
+Created on 2016 máj. 19
+
+@author: Balázs
+'''
+
+class Player():
+    '''
+    Player of EUM
+    '''
+    preferredPosition = None
+    previousPosition = None
+    
+    name = None
+    capability = None
+    position = None
+    salience = None
+    
+    sumOfVotes = 0
+    
+    offers = []
+
+    def __init__(self, name, capability, position, salience):
+        self.name = name
+        self.capability = float(capability)
+        self.position = float(position)
+        self.previousPosition = float(position)
+        self.salience = float(salience)
+        
+        self.preferredPosition = self.position
+        
+    def power(self):
+        return self.capability*self.salience
+    
+    def addToSum(self, value):
+        self.sumOfVotes += value
+        
+    def updatePosition(self, value):
+        self.previousPosition = self.position
+        self.position = value
+    
+    def __str__(self):
+        return ','.join([self.name, str(self.capability), str(round(self.position, 3)), str(self.salience)])
+    def __repr__(self):
+        return self.__str__()
