f350ada246462a6f8577808100595d8ae5601e39
minimalgeek <minimalgeek@gmail.com>
2016-07-18 10:58:22 +0200

    Calc

---
+++ hu/farago/eum2_Cahi/calculator/WeightedPositionCalculator.py
@@ -0,0 +1,17 @@
+'''
+Created on 2016 júl. 18
+
+@author: Balázs
+'''
+
+class WeightedPositionCalculator(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self, params):
+        '''
+        Constructor
+        '''
+        
